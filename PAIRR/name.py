30f2583b8128d544afea122969202ec2b75b804a
Beamsy <jake.dbeams@gmail.com>
2016-11-24 20:47:37 +0000

    Added Caillin to names list

--- PAIRR/name.py
+++ PAIRR/name.py
@@ -1,3 +1,3 @@
-number_student = dict({'31009271': 'Wayne', '37017063': 'Jake', '37033972': 'Ashleigh'})
+number_student = dict({'31009271': 'Wayne', '37017063': 'Jake', '37033972': 'Ashleigh', '36978906': 'Caillin'})
 def name(scan):
     return number_student[scan]
