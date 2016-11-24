30d73c5395c768e686f861eade4a78c37d26c990
Beamsy <jake.dbeams@gmail.com>
2016-11-24 20:46:55 +0000

    Helps to test your code!

--- PAIRR/name.py
+++ PAIRR/name.py
@@ -1,3 +1,3 @@
-number_student = dict({31009271: 'Wayne', 37017063: 'Jake', 37033972: 'Ashleigh'})
+number_student = dict({'31009271': 'Wayne', '37017063': 'Jake', '37033972': 'Ashleigh'})
 def name(scan):
     return number_student[scan]
