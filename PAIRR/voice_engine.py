9fc114db8c7a83aa4a8e4d167e84b4a616b9dbc9
Beamsy <jake.dbeams@gmail.com>
2016-11-24 14:47:58 +0000

    Update voice_engine.py
    Added comments

--- PAIRR/voice_engine.py
+++ PAIRR/voice_engine.py
@@ -1,6 +1,12 @@
+#Import the pyttsx module. This must be installed (sudo pip install pyttsx)
 import pyttsx
+#Create an engine object, this allows us to send commands to the speech engine
 engine = pyttsx.init()
+#Using startLoop with a false parameter allows us to use an external loop
 engine.startLoop(False)
+#This is the only function provided, it simply speaks a string passed to it
 def say(utterance):
+    #This is the actual pass through to the speech engine
     engine.say(utterance)
+    #And this tells the engine to speak the command and move on.
     engine.iterate()
