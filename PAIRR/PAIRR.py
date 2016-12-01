d86f822ef0fa249c859774a5f8df6c8df1c9bf06
Beamsy <jake.dbeams@gmail.com>
2016-12-01 09:54:07 +0000

    Add init and main loop

--- PAIRR/PAIRR.py
+++ PAIRR/PAIRR.py
@@ -1,6 +1,9 @@
 import voice_engine
+from voice_engine import say
 import name
-code = raw_input()
-student = name.name(code)
-greeting = 'Hello there {0}, how are you today?'.format(student)
-voice_engine.say(greeting)
+say("Initiating")
+while True:
+    code = raw_input()
+    student = name.name(code)
+    greeting = 'Hello {0}.'.format(student)
+    say(greeting)
