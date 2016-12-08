93d55f3ac9d253c6ad7e67a99fdd4d37ecc398e8
Beamsy <jake.dbeams@gmail.com>
2016-12-08 10:15:15 +0000

    Small amount of database integration into main codebase.

--- PAIRR/PAIRR.py
+++ PAIRR/PAIRR.py
@@ -1,9 +1,12 @@
 import voice_engine
 from voice_engine import say
-import name
+import db_access
+import student_access
 say("Initiating")
+db = db_access.__init__()
 while True:
     code = raw_input()
-    student = name.name(code)
+    student = student_access.name(db, code)
     greeting = 'Hello {0}.'.format(student)
     say(greeting)
+    print greeting
---
+++ PAIRR/student_access.py
@@ -0,0 +1,14 @@
+from db_access import run_query_single
+
+# This prebaked query can be appended with a StudentNumber, retrieved from
+# a barcode to provide the name of a Student
+query_name = "SELECT `FirstName`,`LastName` FROM `tblstudent` WHERE `StudentNumber` = "
+
+
+# Returns the first name of a student, given their student number
+# Also has to be passed the database object.
+def name(db, code):
+    query = query_name + str(code)
+    retrieved = run_query_single(db, query)[0]
+    return retrieved
+
