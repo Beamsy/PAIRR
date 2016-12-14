8ece8820369e4e134552b1938f7900a5499f95c5
Jake Beams <jake.dbeams@gmail.com>
2016-12-14 20:53:56 +0000

    Commit from Pi - Please Ignore

--- PAIRR/PAIRR.py
+++ PAIRR/PAIRR.py
@@ -2,9 +2,14 @@ import voice_engine
 from voice_engine import say
 import db_access
 import student_access
+import pir_util
 say("Initiating")
+said = False
 db = db_access.__init__()
 while True:
+    '''if pir_util.detect_move() and said is False:
+        say("Hey you there")
+        said = True'''
     code = raw_input()
     student = student_access.name(db, code)
     greeting = 'Hello {0}.'.format(student)
--- PAIRR/PIRTest.py
+++ PAIRR/PIRTest.py
@@ -1,6 +1,6 @@
 # Imports needed to use the GPIO and sleep function from time
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 # Tells the program which numbering scheme you're using for the GPIO pins,
 # BCM is the "Broadcom SOC channel", the name of the pins, as opposed
@@ -8,9 +8,10 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Name the GPIO pins used by the PIR sensor and LED
-pirPin = 7
-ledPin = 6
+pirPin = 18
+ledPin = 23
 
+said = False
 # Set the PIR pin as an input and ledPin as out
 GPIO.setup(pirPin, GPIO.IN)
 GPIO.setup(ledPin, GPIO.OUT)
@@ -20,16 +21,26 @@ GPIO.setup(ledPin, GPIO.OUT)
 print "PIR test"
 print "To stop the program hit CTRL+C"
 print "Ready!"
-
-# Continuous loop listening for an input from the PIR
-while True:
-    # Checks if there's an input from the PIR
-    if GPIO.input(pirPin):
-        print "Motion Detected!"
-        # Turns on ledPin for one second then turns it off
-        GPIO.output(ledPin, True)
-        time.sleep(1)
-        GPIO.output(ledPin, False)
-        time.sleep(2)
-    # waits one second before sensing again.
-    time.sleep(1)
+GPIO.output(ledPin, 1)
+sleep(1)
+GPIO.output(ledPin, 0)
+sleep(1)
+try:
+    # Continuous loop listening for an input from the PIR
+    while True:
+        # Checks if there's an input from the PIR
+        if GPIO.input(pirPin):
+            # Turns on ledPin for one second then turns it off
+            GPIO.output(ledPin, 1)
+            print "Motion!"
+            said = True
+        # waits one second before sensing again.
+        elif not GPIO.input(pirPin) and not said:
+            GPIO.output(ledPin, 0)
+            print "Safe!"
+            said = False
+        sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print "Exiting"
+    exit(0)
