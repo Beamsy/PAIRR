deb0a99fe83457814efa93f68b66bd4b7cbb7449
Beamsy <jake.dbeams@gmail.com>
2017-02-03 14:08:13 +0000

    Fixed selection of highest weighted neuron

--- PAIRR/AI/__init__.py
+++ PAIRR/AI/__init__.py
@@ -1,13 +1,13 @@
 from neuron import Neuron
 from alvl import Alvl
-neuronlist = [Neuron(count) for count in range(3)]
-alvllist = [Alvl(count) for count in range(6)]
-alvls = {1, 3, 5}
-alvllist[5].weights[2] = 0.75
-for i in range(neuronlist.__len__()):
-    neuronlist[i].sum_weighted_inputs(alvls, alvllist)
-for i in range(neuronlist.__len__()):
-    print neuronlist[i].total
-selected = sorted(neuronlist, key=lambda neuron: Neuron.total, reverse=True) #Sorts neurons by reverse weighting
-
-print "Neuron " + str(selected[0].idnum) + ": " + str(selected.total)
+degree_list = NotImplemented
+neuron_list = [Neuron(count) for count in range(3)]
+alvl_list = [Alvl(count) for count in range(6)]
+alvls_studied = {1, 3, 5}
+alvl_list[5].weights[2] = 0.75
+for i in range(neuron_list.__len__()):
+    neuron_list[i].sum_weighted_inputs(alvls_studied, alvl_list)
+for i in range(neuron_list.__len__()):
+    print neuron_list[i].total
+selected = sorted(neuron_list, key=Neuron.get_total, reverse=True)[0]  # Sorts neurons by reverse weighting
+print "Neuron " + str(selected.idnum) + ": " + str(selected.total)
--- PAIRR/AI/neuron.py
+++ PAIRR/AI/neuron.py
@@ -1,13 +1,18 @@
-class Neuron():
+class Neuron:
+
     idnum = None
     total = 0
+
     def __init__(self, pidnum):
         self.idnum = pidnum
         print "neuron " + str(self.idnum) + " initiated"
 
-    def sum_weighted_inputs(self, alvls, alvllist):
+    def sum_weighted_inputs(self, alvls_studied, alvl_list):
         print "Summing neuron " + str(self.idnum)
         total = 0
-        for i in alvls:
-            total = total + alvllist[i].weights[self.idnum]
+        for i in alvls_studied:
+            total = total + alvl_list[i].weights[self.idnum]
         self.total = total
+
+    def get_total(self):
+        return self.total
