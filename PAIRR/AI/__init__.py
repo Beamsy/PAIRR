bfdcd8bb502cc8416afe3b7a0ae4c3ad49dd86a6
Beamsy <jake.dbeams@gmail.com>
2017-02-10 12:13:11 +0000

    Fix weights assignment

--- PAIRR/AI/__init__.py
+++ PAIRR/AI/__init__.py
@@ -1,15 +1,19 @@
 from neuron import Neuron
 from alvl import Alvl
+
 alvl_names = ["Environmental Science", "Media Studies", "French", "Computer Science", "Physics", "Economics"]
 degree_list = ["Animal Conservation", "Human Bio-science", "Computing"]
 neuron_list = [Neuron(count, degree_list[count]) for count in range(degree_list.__len__())]
 alvl_list = [Alvl(count, alvl_names[count]) for count in range(alvl_names.__len__())]
-alvls_studied = {1, 3, 5}
-alvl_list[5].weights[2] = 0.75
+alvls_studied = {1, 2, 4}
+alvl_list[3].weights[2] = 1
+
 for i in range(neuron_list.__len__()):
     neuron_list[i].sum_weighted_inputs(alvls_studied, alvl_list)
+
 for i in range(neuron_list.__len__()):
     print neuron_list[i].total
+
 selected = sorted(neuron_list, key=Neuron.get_total, reverse=True)[0]  # Sorts neurons by reverse weighting
-print "Neuron " + str(selected.idnum) + ": " + str(selected.total)
+print "Neuron " + str(selected.id_num) + ": " + str(selected.total)
 print "You should study: " + selected.identifier
--- PAIRR/AI/alvl.py
+++ PAIRR/AI/alvl.py
@@ -2,12 +2,13 @@ class Alvl:
 
     identifier = None
     id_num = None
-    weights = [0.5 for count in range(3)]
+    weights = None
 
     def __init__(self, _id_num, _identifier):
         print "a level " + str(_id_num) + " initiated"
         self.id_num = _id_num
         self.identifier = _identifier
+        self.weights = [0.5 for count in range(3)]
 
     def __repr__(self):
         return "ALevel: {} with ID {}".format(self.identifier,
