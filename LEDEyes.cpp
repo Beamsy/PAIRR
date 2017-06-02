e1d4a83e5a47f02d65cbb821c290f16e5c034bc4
Caillin Spain <xnc30x@googlemail.com>
2017-06-02 20:20:47 +0100

    ARD - Refactored LEDEyes to use just two LED boards, renamed all the methods and arrays/bytes.

--- LEDEyes.cpp
+++ LEDEyes.cpp
@@ -2,28 +2,12 @@
 
 // This allows the code to use the LedControl library
 #include "LedControl.h"
- 
+
 // Pins: DIN,CLK,CS, number of Display connected
-LedControl lc = LedControl(12, 11, 10, 3);
- 
+LedControl lc = LedControl(12, 11, 10, 1);
 
-// Before we go on, this code is not perfect and was written at midnight, it needs refining and there are better ways to do a lot of it
-// Feel free to add to this!!!
-
-
-// Put the values in arrays for each of the eye/mouth positions
-// Naming convention for arrays: 
-// L = look left
-// R = look right
-// D = look down
-// U = look up
-// 0 = left eye, or the first LED board
-// 1 = right eye, or the second LED board
-// The number at the end relates to how far across the eye moves in the given direction - L0R11 == as below but 1 step in from the edge
-// So the first array, L0R1 = look left for the left eye/first board -- look right for the right eye/second board
-// The reason for the backwards naming is because the name can't begin with a number
-// Eye Arrays
-byte L0R1[] =
+// Eye Position Arrays
+byte right[] =
 {
 	B00111100,
 	B01100110,
@@ -35,7 +19,7 @@ byte L0R1[] =
 	B00111100
 };
 
-byte R0L1[] =
+byte left[] =
 {
 	B00111100,
 	B01111110,
@@ -47,7 +31,7 @@ byte R0L1[] =
 	B00111100
 };
 
-byte L0R11[] =
+byte right1[] =
 {
 	B00111100,
 	B01111110,
@@ -59,7 +43,7 @@ byte L0R11[] =
 	B00111100
 };
 
-byte R0L11[] =
+byte left1[] =
 {
 	B00111100,
 	B01111110,
@@ -71,7 +55,7 @@ byte R0L11[] =
 	B00111100
 };
 
-byte centerEye[] =
+byte center[] =
 {
 	B00111100,
 	B01111110,
@@ -83,7 +67,7 @@ byte centerEye[] =
 	B00111100
 };
 
-byte U0D1[] =
+byte down[] =
 {
 	B00111100,
 	B01111110,
@@ -95,7 +79,7 @@ byte U0D1[] =
 	B00111100
 };
 
-byte U0D11[] =
+byte down1[] =
 {
 	B00111100,
 	B01111110,
@@ -107,7 +91,7 @@ byte U0D11[] =
 	B00111100
 };
 
-byte D0U1[] =
+byte up[] =
 {
 	B00111100,
 	B01111110,
@@ -119,7 +103,7 @@ byte D0U1[] =
 	B00111100
 };
 
-byte D0U11[] =
+byte up1[] =
 {
 	B00111100,
 	B01111110,
@@ -131,7 +115,7 @@ byte D0U11[] =
 	B00111100
 };
 
-byte eyeBlink[] =
+byte blink[] =
 {
 	B00011000,
 	B00011000,
@@ -144,259 +128,147 @@ byte eyeBlink[] =
 };
 
 
-// Arrays for the mouth
-byte unhappy[] =
-{
-	B10000001,
-	B11000011,
-	B10111101,
-	B10000001,
-	B01000010,
-	B00111100,
-	B00000000,
-	B00000000
-};
-
-byte unhappy1[] =
-{
-	B00000000,
-	B00000000,
-	B11111111,
-	B10000001,
-	B01000010,
-	B00111100,
-	B00000000,
-	B00000000
-};
-
-byte happy1[] =
-{
-	B00000000,
-	B00000000,
-	B00111100,
-	B01000010,
-	B10000001,
-	B11111111,
-	B00000000,
-	B00000000
-};
-
-byte happy[] =
-{
-	B00000000,
-	B00000000,
-	B00111100,
-	B01000010,
-	B10000001,
-	B10111101,
-	B11000011,
-	B10000001
-};
-
+// Constructor, set up LED boards and serial connection
 LEDEyes::LEDEyes()
 {
 	// Wake up displays
 	lc.shutdown(0, false);
-	lc.shutdown(1, false);
-	lc.shutdown(2, false);
 
 	// Set intensity levels
 	lc.setIntensity(0, 5);
-	lc.setIntensity(1, 5);
-	lc.setIntensity(2, 5);
 
 	// Clear Displays
 	lc.clearDisplay(0);
-	lc.clearDisplay(1);
-	lc.clearDisplay(2);
+
+	restTime = 3000;
+	delayTime = 1000;
+	pauseTime = 200;
+	moveTime = 50;
 
 	Serial.begin(9600);
 }
 
 //  Take values stored in the Arrays, loop through and Display them
-void LEDEyes::leftEyeLeft()
+void LEDEyes::lookUp()
 {
 	for (int i = 0; i < 8; i++)
 	{
 		// Set the row of the given LED board(the 1st param),
 		// using the given array(3rd param)
-		lc.setRow(0, i, L0R1[i]);
+		lc.setRow(0, i, up[i]);
 	}
 }
 
-void LEDEyes::rightEyeLeft()
+void LEDEyes::lookUp1()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(1, i, R0L1[i]);
+		lc.setRow(0, i, up1[i]);
 	}
 }
 
-void LEDEyes::leftEyeRight()
+void LEDEyes::lookDown()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(0, i, R0L1[i]);
+		lc.setRow(0, i, down[i]);
 	}
 }
 
-void LEDEyes::rightEyeRight()
+void LEDEyes::lookDown1()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(1, i, L0R1[i]);
+		lc.setRow(0, i, down1[i]);
 	}
 }
 
-void LEDEyes::lEleft1()
+void LEDEyes::lookLeft()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(0, i, L0R11[i]);
+		lc.setRow(0, i, left[i]);
 	}
 }
 
-void LEDEyes::centerL()
+void LEDEyes::lookLeft1()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(0, i, centerEye[i]);
+		lc.setRow(0, i, left1[i]);
 	}
 }
 
-void LEDEyes::centerR()
+void LEDEyes::lookRight()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(1, i, centerEye[i]);
+		lc.setRow(0, i, right[i]);
 	}
 }
 
-// Methods to move left eye 
-void LEDEyes::lEright1()
+void LEDEyes::lookRight1()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(0, i, R0L11[i]);
+		lc.setRow(0, i, right1[i]);
 	}
 }
 
-void LEDEyes::rEleft1()
+void LEDEyes::eyeCenter()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(1, i, R0L11[i]);
+		lc.setRow(0, i, center[i]);
 	}
 }
 
-void LEDEyes::rEright1()
+void LEDEyes::eyeBlink()
 {
 	for (int i = 0; i < 8; i++)
 	{
-		lc.setRow(1, i, L0R11[i]);
+		lc.setRow(0, i, blink[i]);
 	}
 }
 
-void LEDEyes::lEUp()
+void LEDEyes::eyesAwake()
 {
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(0, i, U0D1[i]);
-	}
+	eyeCenter();
+	delay(delayTime);
+	lookLeft1();
+	delay(moveTime);
+	lookLeft();
+	delay(pauseTime);
+	lookLeft1();
+	delay(moveTime);
+	eyeCenter();
+	delay(moveTime);
+	lookRight1();
+	delay(moveTime);
+	lookRight();
+	delay(pauseTime);
+	lookRight1();
+	delay(moveTime);
+	eyeCenter();
+	delay(delayTime);
+	eyeBlink();
+	delay(pauseTime);
+	eyeCenter();
 }
 
-void LEDEyes::rEDown()
+void LEDEyes::eyesRest()
 {
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(1, i, U0D1[i]);
-	}
-}
-
-void LEDEyes::rEDown1()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(1, i, U0D11[i]);
-	}
-}
-
-void LEDEyes::lEDown()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(0, i, D0U1[i]);
-	}
-}
-
-void LEDEyes::lEDown1()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(0, i, D0U11[i]);
-	}
-}
-
-void LEDEyes::leftEyeBlink()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(0, i, eyeBlink[i]);
-	}
-}
-
-void LEDEyes::rightEyeBlink()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(1, i, eyeBlink[i]);
-	}
-}
-
-
-// Methods for moving the mouth
-void LEDEyes::mouthUnhappy()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(2, i, unhappy[i]);
-	}
-}
-
-void LEDEyes::mouthHappy()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(2, i, happy[i]);
-	}
-}
-
-void LEDEyes::mouthHappy1()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(2, i, happy1[i]);
-	}
-}
-
-void LEDEyes::mouthUnhappy1()
-{
-	for (int i = 0; i < 8; i++)
-	{
-		lc.setRow(2, i, unhappy1[i]);
-	}
-}
-
-void LEDEyes::faceAwake()
-{
-	mouthHappy();
-	centerL();
-	centerR();
-}
-
-void LEDEyes::faceAsleep()
-{
-	mouthHappy();
-	leftEyeBlink();
-	rightEyeBlink();
+	eyeCenter();
+	delay(restTime);
+	eyeBlink();
+	delay(pauseTime);
+	eyeCenter();
+	delay(restTime);
+	eyeBlink();
+	delay(pauseTime);
+	eyeCenter();
+	delay(pauseTime);
+	eyeBlink();
+	delay(pauseTime);
+	eyeCenter();
 }
--- LEDEyes.h
+++ LEDEyes.h
@@ -6,41 +6,28 @@
 class LEDEyes
 {
 
-
 public:
 	// Constructor
 	LEDEyes();
 
-	// Destructor - deletes the object
-	~LEDEyes();
+	void lookLeft();
+	void lookLeft1();
+	void lookRight();
+	void lookRight1();
+	void lookUp();
+	void lookUp1();
+	void lookDown();
+	void lookDown1();
+	void eyeCenter();
+	void eyeBlink();
 
-	void leftEyeLeft();
-	void rightEyeLeft();
-	void leftEyeRight();
-	void rightEyeRight();
-	void lEleft1();
-	void centerL();
-	void centerR();
-	void lEright1();
-	void rEleft1();
-	void rEright1();
-	void lEUp();
-	void rEDown();
-	void rEDown1();
-	void lEDown();
-	void lEDown1();
-	void leftEyeBlink();
-	void rightEyeBlink();
-	void mouthUnhappy();
-	void mouthHappy();
-	void mouthHappy1();
-	void mouthUnhappy1();
+	void eyesAwake();
+	void eyesRest();
 
-	void faceHappy();
-	void faceBlink();
-	void faceAsleep();
-	void faceAwake();
-	void faceSad();
-
+private:
+	unsigned long delayTime;
+	unsigned long pauseTime;
+	unsigned long moveTime;
+	unsigned long restTime;
 };
 #endif
